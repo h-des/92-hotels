96aeb270653f474b84e7e9af1a61f45d875cac84
h-des <patryk.holysz@gmail.com>
2018-09-23 17:54:42 +0200

    move css from index.css to injectGlobal in index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight: 400;
+}
+`
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
