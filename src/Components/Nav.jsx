f3f6d7e0fc77765335326fcf256b43a46ad4463f
h-des <patryk.holysz@gmail.com>
2018-11-29 08:59:24 +0100

    change navbar from grid to flexbox

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,20 +12,30 @@ import * as actions from '../actions';
 const breakPoint = '425px';
 
 const StyledNav = styled.nav`
-  display: grid;
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   z-index: 1000;
   width: 100%;
-  grid-template-columns: max-content 1fr min-content;
-  grid-template-rows: 60px 0px;
   background-color: white;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
 
-  @media only screen and (max-width: ${breakPoint}) {
-    grid-template-columns: max-content 1fr min-content;
-    grid-template-rows: 60px max-content;
+  @media only screen and (max-width: 425px) {
   }
 `;
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 425px) {
+    flex-direction: column;
+    width: 100%;
+  }
+`;
+
 const NavHamburger = styled.button`
   outline: none;
   background-color: transparent;
@@ -45,7 +55,6 @@ const NavHamburger = styled.button`
 `;
 
 const NavLogo = styled(Link)`
-  grid-column: 1 / span 1;
   align-self: center;
   font-weight: 900;
   font-size: 1.8rem;
@@ -54,7 +63,6 @@ const NavLogo = styled(Link)`
   color: #0c0c0c;
 
   @media only screen and (max-width: ${breakPoint}) {
-    grid-column: 2 / span 1;
     text-align: center;
   }
 `;
@@ -70,8 +78,10 @@ const NavItems = styled.ul`
   justify-self: right;
 
   @media only screen and (max-width: ${breakPoint}) {
-    grid-row: 2/3;
-    grid-column: 1/-1;
+    position: absolute;
+    top: 54px;
+    background-color: white;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
     display: ${props => (props.show ? 'flex' : 'none')};
     flex-direction: column;
     justify-self: center;
@@ -88,8 +98,10 @@ const LoginButton = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
+
   @media only screen and (max-width: ${breakPoint}) {
-    width: 100%;
+    /* width: 100%; */
+
     text-align: center;
     margin-left: 0;
     margin-right: 0.5rem;
@@ -176,12 +188,17 @@ class Nav extends Component {
         <NavHamburger onClick={() => this.toggleList()}>
           <NavIcon src={hamburgerIcon} />
         </NavHamburger>
-        <NavLogo to="/">
-          <HideSpan>Hotel Black</HideSpan>92
-        </NavLogo>
-        <NavItems onClick={() => this.toggleList()} show={this.state.showList}>
-          {this.renderList()}
-        </NavItems>
+        <NavContainer>
+          <NavLogo to="/">
+            <HideSpan>Hotel Black</HideSpan>92
+          </NavLogo>
+          <NavItems
+            onClick={() => this.toggleList()}
+            show={this.state.showList}
+          >
+            {this.renderList()}
+          </NavItems>
+        </NavContainer>
         {this.props.user.data ? (
           <LoginButton onClick={this.props.logOut}>Logout</LoginButton>
         ) : (
